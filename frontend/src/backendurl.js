7759b218fb053ff5a33ae4dea6d77bfa82e265c2
andavarapulikith <likithandavarapuiiits@gmail.com>
2024-07-05 15:39:33 +0530

    changed backend url

--- frontend/src/backendurl.js
+++ frontend/src/backendurl.js
@@ -1,3 +1,3 @@
-// export const backendurl = "http://65.0.110.134:5000"
-export const backendurl="https://codebuilders.onrender.com"
+export const backendurl = "https://backend.likith.online"
+// export const backendurl="https://codebuilders.onrender.com"
 // export const backendurl="http://localhost:5000"
